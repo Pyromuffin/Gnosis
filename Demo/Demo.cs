7353bb43beb8dfce08c1aa644d2c559914bc362e
Kelly MacNeill <kelly@pyromuffin.com>
2020-04-07 00:40:07 -0700

    initial commit

---
+++ Demo/Demo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Gnosis;
+using static Gnosis.Rule;
+
+namespace Demo
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            Demo1 demo = new Demo1();
+            demo.Start();
+
+            // lets write some inform!
+            var inform = new Inform();
+            var output = inform.OutputInform();
+            var path = @"C:\Users\pyrom\Documents\GitHub\Inform Gen Test\Gen Test.materials\Extensions\Kelly MacNeill\Generated.I7X";
+
+            System.IO.File.WriteAllText(path, output);
+            Console.WriteLine(output);
+        }
+    }
+
+
+    public class Demo1
+    {
+        public void Start()
+        {
+            Room kitchen = new Room("The Kitchen");
+            kitchen.description =
+       @"I'm in the kitchen now. [if the lockdown state is true] A pleasant yet urgent announcement rings '[b]Laserite Core criticality event detected: apartment lockdown in effect.[/b]' The doors leading to other parts of the apartment are shielded with comically elaborate folding door layers.[end if]
+        [if unvisited]The refrigerator remains unmentioned.[end if]
+        A [table] obnoxiously takes up most of the useful room in here.";
+
+            Room yourRoom = new Room("Your Room");
+            yourRoom.description =
+       @"I'm in your room [if unvisited]now.[otherwise]again.[end if] What do you want me to do in here? You obviously know what's in your room, but just in case[one of] you have amnesia[or] you're standing on a rug that erases all memory of your existence outside the rug[or] a cougar ate your memory of being attacked by a cougar[or] i'm talking to a stranger who stole your phone[at random]: You've got your desk with wires growing out of it. There are some seltzer bottles strewn on the floor. It's kind of a mess in here tbh.";
+            kitchen.connections.Add((Direction.West, yourRoom));
+
+
+            Thing cellphone = new Thing("The cellphone");
+            cellphone.description = "It's my fancy new iPhone LXVMMMMCDXX - and the object that i'm currently using to text you. In airplane mode, it hovers at a convenient distance to provide a truly hands-free experience.";
+            cellphone.aliases.Add("phone");
+            cellphone.aliases.Add("iphone LXVMMMMCDXX/--");
+            kitchen.things.Add(cellphone);
+
+            Supporter kitchenTable = new Supporter("The kitchen table");
+            kitchen.things.Add(kitchenTable);
+            kitchenTable.description = "An unlikely place to put anything useful.";
+
+
+            Thing chonkChart = new Thing("The chonk chart");
+            chonkChart.description =
+                @"A small holocard with a guady label that says '[b]Now That's What I Call Chonk! Vol. 1[/b]'
+                I think I can use this with the Chonkédex app on my [cellphone].";
+
+            kitchenTable.things.Add(chonkChart);
+
+            UnaryAction taking = new UnaryAction("insulting", "insult [something]");
+
+            Instead(taking).Condition((Gnobject a) => a == chonkChart).Say("I lean over the[chart], but my[cellphone] zips in and absorbs the holocard before i can chow down.");
+        }
+    }
+
+}
---
+++ Gnosis/Gnosis.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using static Gnosis.Rule;
+
+namespace Gnosis
+{
+    // http://cs.gettysburg.edu/~tneller/fys187-4/Inform7_CheatSheet.pdf
+
+
+    public enum Direction
+    {
+        North,
+        West,
+        East,
+        South,
+        Up,
+        Down,
+        Inside,
+        Outside
+    }
+
+
+
+    public static class Globals
+    {
+        static public List<Room> rooms = new List<Room>();
+        static public List<Action> actions = new List<Action>();
+        static public Room location;
+    }
+
+
+    public class Gnobject
+    {
+
+
+    }
+
+    public class Room : Gnobject
+    {
+        public bool lit = true;
+        public bool visited = false;
+        public string name;
+        public string description;
+        public Gnobject region;
+        public List<(Direction, Room)> connections = new List<(Direction, Room)>();
+        public List<Thing> things = new List<Thing>();
+
+        public Room(string name)
+        {
+            this.name = name;
+            Globals.rooms.Add(this);
+        }
+    }
+
+
+    public class Thing : Gnobject
+    {
+        public bool edible = false;
+        public bool lightProducing = false;
+        public bool portable = true;
+        public bool described = true;
+        public bool markedForListing = false;
+        public bool mentioned = true;
+
+        public bool scenery = false;
+        public bool wearable = false;
+        public bool handled = false;
+        public bool pushable = false;
+
+        public string description;
+        public string initialAppearance;
+        public string name;
+
+        public Gnobject matchingKey;
+        public List<string> aliases = new List<string>();
+        public List<System.Type> props = new List<System.Type>(); 
+
+        public Thing(string name)
+        {
+            this.name = name;
+        }
+
+        public void Is<T>()
+        {
+            props.Add(typeof(T));
+        }
+
+
+    }
+
+    public class Supporter : Thing
+    {
+        public Supporter(string name) : base(name)
+        {
+            portable = false;
+            scenery = true;
+        }
+
+        public List<Thing> things = new List<Thing>();
+    }
+
+    public class Action {
+        // actions ONLY apply to things, eg. a wet thing, a visible thing
+        // OR kinds of value (enums), eg North, chonkedex app 
+
+
+        public Action(string name, string verb)
+        {
+            this.name = name;
+            verbs.Add(verb);
+            Globals.actions.Add(this);
+        }
+
+        public List<string> verbs = new List<string>();
+        public string name;
+    };
+
+    public class SelfAction : Action
+    {
+        public SelfAction(string name, string verb) : base(name, verb) { }
+    }
+
+    public class UnaryAction : Action
+    {
+
+        public UnaryAction(string name, string verb) : base(name, verb) { }
+        public Expression<Predicate<Thing>> applyingTo = (Thing a) => a is Thing;
+    }
+
+    public class BinaryAction : Action
+    {
+        public BinaryAction(string name, string verb) : base(name, verb) { }
+        public Expression<Predicate<Thing>> applyingToFirst = (Thing a) => a is Thing && a.portable;
+        public Expression<Predicate<Thing>> applyingToSecond = (Thing a) => a is Thing;
+    }
+
+    public class Rule
+    {
+        public enum Type
+        {
+            Before,
+            Instead,
+            Check,
+            CarryOut,
+            After,
+            Report
+        }
+
+        public static List<Rule> rules = new List<Rule>();
+
+        public Type type;
+
+        public Action applyingTo;
+        public List<Gnobject> targets = new List<Gnobject>();
+        public List<Room> locations = new List<Room>();
+        public Expression<Predicate<Gnobject>> condition;
+        public List<string> say = new List<string>();
+
+        public Rule()
+        {
+            rules.Add(this);
+        }
+
+
+        public static Rule Instead(Action action)
+        {
+            var rule = new Rule();
+            rule.type = Type.Instead;
+            rule.applyingTo = action;
+
+            return rule;
+        }
+
+
+        public Rule Target(Gnobject obj)
+        {
+            targets.Add(obj);
+            return this;
+        }
+
+        public Rule Where(Room room)
+        {
+            locations.Add(room);
+            return this;
+        }
+
+        public Rule Condition(Expression<Predicate<Gnobject>> expr )
+        {
+            condition = expr;
+            return this;
+        }
+
+        public Rule Do(Func<bool> result)
+        {
+
+            return this;
+        }
+
+        public Rule Say(string str)
+        {
+            say.Add(str);
+            return this;
+        }
+
+
+    }
+
+    public class Inform
+    {
+        string OutputRoom(Room room)
+        {
+            var s = room.name + " is a room. The description is \"" + room.description + "\"\n";
+            foreach(var connection in room.connections)
+            {
+                s += connection.Item1.ToString() + " of " + room.name + " is " + connection.Item2.name + ".\n";
+            }
+            return s;
+        }
+
+        string OutputThing(Thing thing, Room location, Thing container)
+        {
+            var s = "";
+            if(location != null)
+                s = thing.name + " is a " + thing.GetType().Name + " in " + location.name + ". ";
+
+            if(container != null)
+                s = thing.name + " is a " + thing.GetType().Name + " on " + container.name + ". ";
+
+            s += "The description is \"" + thing.description + "\"\n";
+
+
+            foreach(var alias in thing.aliases)
+            {
+                s += "Understand \"" + alias + "\" as " + thing.name + ".\n";
+            }
+
+
+            if(thing is Supporter supporter)
+            {
+                foreach(var subthing in supporter.things)
+                {
+                    s += OutputThing(subthing, null, supporter);
+                }
+            }
+
+            return s;
+        }
+
+
+
+
+        public string OutputInform()
+        {
+            var output = "Generated by Kelly MacNeill begins here.\n";
+
+            foreach (var room in Globals.rooms)
+            {
+                output += OutputRoom(room);
+    
+                foreach (var thing in room.things)
+                {
+                    output += OutputThing(thing, room, null);
+                }
+            }
+
+
+            output += "Generated ends here.\n";
+
+            return output;
+        }
+    }
+}
